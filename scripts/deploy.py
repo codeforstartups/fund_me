98a8e66f1a14736fd55b2df9a3408e5aa750824c
codeforstartups <48567820+codeforstartups@users.noreply.github.com>
2021-12-19 15:47:23 +0530

    Updated with Mock , Contract Deployed

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,17 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
+from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
-    pass
+    account = get_account()
+    # if we are on persistent network like rinkby, use the assosiated address
+    # otherwise deploy mocks
+    if network.show_active() == "developement":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"]
+    fund_me = FundMe.deploy("0x8A753747A1Fa494EC906cE90E9f37563A8AF630e", {
+                            "from": account}, publish_source=True)
+    print(f"Contract Deployed to {fund_me.address}")
 
 
 def main():
